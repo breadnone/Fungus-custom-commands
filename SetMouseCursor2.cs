fec807978e9de1b3a1fe98f9c26c54fd52eac39e
breadnone <64100867+breadnone@users.noreply.github.com>
2020-12-14 22:36:07 +0700

    Create SetMouseCursor2.cs
    Better Set Mouse Cursor. 2 Images needed. One for default and the other for when clicking

---
+++ SetMouseCursor2.cs
@@ -0,0 +1,96 @@
+// This code is part of the Fungus library (https://github.com/snozbot/fungus)
+// It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
+
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Fungus
+{
+    /// <summary>
+    /// Sets the mouse cursor sprite.
+    /// </summary>
+    [CommandInfo("Sprite", 
+                 "Set Mouse Cursor 2", 
+                 "Sets the mouse cursor sprite.")]
+    [AddComponentMenu("")]
+    public class SetMouseCursor2 : Command 
+    {
+        [Tooltip("Texture to use for cursor. Will use default mouse cursor if no sprite is specified")]
+        [SerializeField] protected Texture2D cursorTexture;
+
+        [Tooltip("The offset from the top left of the texture to use as the target point")]
+        [SerializeField] protected Vector2 hotSpot;
+
+        [Tooltip("Texture to use for cursor. Will use default mouse cursor if no sprite is specified")]
+        [SerializeField] protected Texture2D cursorTexture2;
+
+        [Tooltip("The offset from the top left of the texture to use as the target point")]
+        [SerializeField] protected Vector2 hotSpot2;
+        // Cached static cursor settings
+        protected static Texture2D activeCursorTexture;
+        protected static Vector2 activeHotspot;
+        protected static Texture2D activeCursorTexture2;
+        protected static Vector2 activeHotspot2;
+        protected float delayInput = 0;
+        protected bool clicked = false;
+        #region Public members
+
+        public static void ResetMouseCursor()
+        {
+            // Change mouse cursor back to most recent settings
+            Cursor.SetCursor(activeCursorTexture, activeHotspot, CursorMode.Auto);
+            Cursor.SetCursor(activeCursorTexture2, activeHotspot2, CursorMode.Auto);
+        }
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(clicking());
+            } 
+        }
+        public IEnumerator clicking()
+        {
+            Cursor.SetCursor(activeCursorTexture2, activeHotspot2, CursorMode.Auto);
+            yield return new WaitForSeconds(0.2f);
+            BeforeClicking();
+
+        }
+        public void BeforeClicking()
+        {
+            Cursor.SetCursor(activeCursorTexture, activeHotspot, CursorMode.Auto);
+        }
+
+        public override void OnEnter()
+        {
+            Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+            Cursor.SetCursor(cursorTexture2, hotSpot2, CursorMode.Auto);
+            activeCursorTexture = cursorTexture;
+            activeCursorTexture2 = cursorTexture2;
+            activeHotspot = hotSpot;
+            activeHotspot2 = hotSpot2;
+            Continue();
+        }
+
+        public override string GetSummary()
+        {
+            if (cursorTexture == null)
+            {
+                return "Error: No cursor sprite selected";
+            }
+            if (cursorTexture2 == null)
+            {
+                return "Error: No cursor sprite selected";
+            }
+
+            return cursorTexture.name + " : " + cursorTexture2.name;
+        }
+
+        public override Color GetButtonColor()
+        {
+            return new Color32(235, 191, 217, 255);
+        }
+
+        #endregion
+    }
+}
