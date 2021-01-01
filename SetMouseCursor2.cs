d890ac72adb8f758565d765ddc14626fc052b242
breadnone <64100867+breadnone@users.noreply.github.com>
2021-01-01 22:19:45 +0700

    Reverted back, again!

--- SetMouseCursor2.cs
+++ SetMouseCursor2.cs
@@ -1,31 +1,24 @@
 // This code is part of the Fungus library (https://github.com/snozbot/fungus)
 // It is released for free under the MIT open source license (https://github.com/snozbot/fungus/blob/master/LICENSE)
 
-using UnityEngine;
+﻿using UnityEngine;
 using System.Collections;
 
 namespace Fungus
 {
-    public enum SetMouseCur
-    {
 
-        Enable,
-        Disable
-
-    }
 
 
     /// <summary>
     /// Sets the mouse cursor sprite.
     /// </summary>
-    [CommandInfo("Sprite",
-                 "Set Mouse Cursor 2",
+    [CommandInfo("Sprite", 
+                 "Set Mouse Cursor 2", 
                  "Sets the mouse cursor sprite.")]
     [AddComponentMenu("")]
-    public class SetMouseCursor2 : Command
+    public class SetMouseCursor2 : Command 
     {
-        [Tooltip("Enable/Disable custom cursor")]
-        [SerializeField] protected SetMouseCur setCustomCursor;
+
         [Tooltip("Texture to use for cursor. Will use default mouse cursor if no sprite is specified")]
         [SerializeField] protected Texture2D cursorTexture;
 
@@ -37,7 +30,7 @@ namespace Fungus
 
         [Tooltip("The offset from the top left of the texture to use as the target point")]
         [SerializeField] protected Vector2 hotSpot2;
-
+        
         [Tooltip("This for safety reasons, in case user spam clicks. Set the number higher. Can't be lower than 0.3, if it's lower, then 0.3 will be used")]
         [SerializeField] protected float clickSpamPrevention = 0.3f;
         private static bool onCustomCursorEnable = false;
@@ -58,30 +51,26 @@ namespace Fungus
 
         void Update()
         {
-            if (Input.GetMouseButtonDown(0))
-            {
-                if (onCustomCursorEnable == true)
-                {
-                    if (!clicked)
-                    {
-                        StartCoroutine(clicking());
-                    }
-                }
 
-            }
+
+                    if (Input.GetMouseButtonDown(0))
+                    {
+                        if(!clicked)
+                        {
+                            StartCoroutine(clicking());
+                        }
+                    }
+                
+            
         }
-        public void ResetMouseToDefault()
-        {
-            onCustomCursorEnable = false;
-            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
-        }
+
         public IEnumerator clicking()
         {
-            if (clicked == false)
+            if(clicked == false)
             {
                 clicked = true;
                 Cursor.SetCursor(activeCursorTexture2, activeHotspot2, CursorMode.Auto);
-                if (clickSpamPrevention <= 0.3)
+                if(clickSpamPrevention <= 0.3)
                 {
                     yield return new WaitForSeconds(0.3f);
                 }
@@ -92,10 +81,10 @@ namespace Fungus
                 BeforeClicking();
             }
         }
-
+        
         public void BeforeClicking()
         {
-            if (clicked == true)
+            if(clicked == true)
             {
                 clicked = false;
                 Cursor.SetCursor(activeCursorTexture, activeHotspot, CursorMode.Auto);
@@ -105,48 +94,34 @@ namespace Fungus
 
         public override void OnEnter()
         {
-            if (setCustomCursor == SetMouseCur.Enable)
+            if (cursorTexture && cursorTexture2 != null)
             {
-                if (cursorTexture && cursorTexture2 != null)
-                {
-                    Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
-                    Cursor.SetCursor(cursorTexture2, hotSpot2, CursorMode.Auto);
-                    activeCursorTexture = cursorTexture;
-                    activeCursorTexture2 = cursorTexture2;
-                    activeHotspot = hotSpot;
-                    activeHotspot2 = hotSpot2;
-                }
-                else
-                {
-                    return;
-                }
-            }
-            if (setCustomCursor == SetMouseCur.Disable)
-            {
-                ResetMouseToDefault();
+                Cursor.SetCursor(cursorTexture, hotSpot, CursorMode.Auto);
+                Cursor.SetCursor(cursorTexture2, hotSpot2, CursorMode.Auto);
+                activeCursorTexture = cursorTexture;
+                activeCursorTexture2 = cursorTexture2;
+                activeHotspot = hotSpot;
+                activeHotspot2 = hotSpot2;
             }
 
+
             Continue();
-        }
+        }        
 
         public override string GetSummary()
         {
-            string acur = "";
-            string bcur = "";
 
-            if (setCustomCursor == SetMouseCur.Enable)
-            {
                 if (cursorTexture == null)
                 {
-                    acur = "Error: No cursor sprite selected";
+                    return "Error: No cursor sprite selected";
                 }
                 if (cursorTexture2 == null)
                 {
-                    bcur = "Error: No cursor sprite selected";
+                    return "Error: No cursor sprite selected";
                 }
-            }
+            
 
-            return acur + " : " + bcur;
+            return cursorTexture.name + " : " + cursorTexture2.name;
         }
 
         public override Color GetButtonColor()
